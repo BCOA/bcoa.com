8584f18858d432dc5c918353acf17e9d98ab9dda
Jake Dobson <jake.dobson@gmail.com>
2018-03-07 08:56:24 -0800

    edit gatsby-config.js -- removed module.exports

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,17 @@
-module.exports = {
-	plugins: [
-		`gatsby-plugin-sass`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `pages`,
-				path: `${__dirname}/src/pages/`,
-			},
+plugins: [
+	`gatsby-plugin-sass`,
+	{
+		resolve: `gatsby-source-filesystem`,
+		options: {
+			name: `pages`,
+			path: `${__dirname}/src/pages/`,
 		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `data`,
-				path: `${__dirname}/src/data/`,
-			},
+	},
+	{
+		resolve: `gatsby-source-filesystem`,
+		options: {
+			name: `data`,
+			path: `${__dirname}/src/data/`,
 		},
-	]
-}
+	},
+]
