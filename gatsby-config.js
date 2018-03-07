36b36e51da32e779bfaa861383f9810719c4b6dd
Jake Dobson <jake.dobson@gmail.com>
2018-03-07 08:42:40 -0800

    project configuration

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	plugins: [
+		`gatsby-plugin-sass`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `pages`,
+				path: `${__dirname}/src/pages/`,
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `data`,
+				path: `${__dirname}/src/data/`,
+			},
+		},
+	];
+}
