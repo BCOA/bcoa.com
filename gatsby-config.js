964abc650844c8229dd599a0b6e5e99317be3055
Jake Dobson <jake.dobson@gmail.com>
2018-03-07 11:29:46 -0800

    deleted node_modules and package-lock then readded for sass plugin to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,19 @@
-plugins: [
-	`gatsby-plugin-sass`,
-	{
-		resolve: `gatsby-source-filesystem`,
-		options: {
-			name: `pages`,
-			path: `${__dirname}/src/pages/`,
+module.exports = {
+	plugins: [
+		`gatsby-plugin-sass`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `pages`,
+				path: `${__dirname}/src/pages/`,
+			},
 		},
-	},
-	{
-		resolve: `gatsby-source-filesystem`,
-		options: {
-			name: `data`,
-			path: `${__dirname}/src/data/`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `data`,
+				path: `${__dirname}/src/data/`,
+			},
 		},
-	},
-]
+	]
+}
