70388b4e7414b1b2b6e118600038b189ceb0c58f
Jake Dobson <jake.dobson@gmail.com>
2018-03-15 08:29:07 -0700

    added plugins for react, added gatsby-source-filesystem for src/pages/projects to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,22 @@
 module.exports = {
 	plugins: [
+		'gatsby-plugin-react-helmet',
 		`gatsby-plugin-sass`,
 		`gatsby-plugin-netlify-cms`,
 		`gatsby-plugin-netlify`,
+		'gatsby-link',
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `pages`,
 				path: `${__dirname}/src/pages/`,
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `projects`,
+				path: `${__dirname}/src/pages/projects/`,
 			},
 		},
 	]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,14 @@
 import React from "react";
+import Link from 'gatsby-link';
 import './index.scss';
 
-export default () => <div>Hello world!</div>
+export default () => 
+
+	<div>
+		<h1>Hello world!</h1>
+		<ul>
+			<li>
+				<Link to="/pages/projects">Projects</Link>
+			</li>
+		</ul>
+	</div>
