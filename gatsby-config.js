a1b264a1a92765b9e046a14b490f9dad7b6c97c7
Josh Dobson <joshua@joshuarule.com>
2018-03-26 11:57:11 -0400

    programmatically create pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,24 @@
 module.exports = {
-	plugins: [
-		'gatsby-plugin-react-helmet',
-		`gatsby-plugin-sass`,
-		`gatsby-plugin-netlify-cms`,
-		`gatsby-plugin-netlify`,
-		'gatsby-link',
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `pages`,
-				path: `${__dirname}/src/pages/`,
-			},
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `projects`,
-				path: `${__dirname}/src/pages/projects/`,
-			},
-		},
-	]
-}
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`,
+    "gatsby-link",
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/pages/projects/`
+      }
+    }
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,45 @@
 import React from "react";
-import Link from 'gatsby-link';
-import './index.scss';
+import Link from "gatsby-link";
+import "./index.scss";
 
-export default () => 
+export default ({ data }) => (
+  <div>
+    <h1>Links</h1>
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link
+          to={node.fields.slug}
+          css={{ textDecoration: `none`, color: `inherit` }}
+        >
+          <h3>
+            {node.frontmatter.title}{" "}
+            <span color="#BBB">— {node.frontmatter.date}</span>
+          </h3>
+        </Link>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
+  </div>
+);
 
-	<div>
-		<h1>Links</h1>
-		<ul>
-			<li>
-				<Link to="/pages/projects">Projects</Link>
-				<Link to="/pages">Pages</Link>
-			</li>
-		</ul>
-	</div>
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
