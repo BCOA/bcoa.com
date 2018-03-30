0f4cd1c8700e25e3d4a01158ff0bf0bd571dcd68
Jake Dobson <jake.dobson@gmail.com>
2018-03-29 19:22:32 -0700

    trying this out from example

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,8 @@
 import CMS from "netlify-cms";
+import { SlidesControl, SlidesPreview } from "./Slides";
+CMS.registerWidget("slides", SlidesControl, SlidesPreview);
+
+/*
 import { ImgFull } from "./imgFull";
 CMS.registerWidget("imgFull", ImgFull);
+*/
