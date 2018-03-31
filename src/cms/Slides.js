6b19ea56fc561b3c73e7b50d09ea8b93f56bf411
Josh Dobson <joshua@joshuarule.com>
2018-03-31 11:17:14 -0400

    edits cms config

--- src/cms/Slides.js
+++ src/cms/Slides.js
@@ -1,15 +1,64 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
-// This is the editing component
+const SlideControlHeader = styled.div`
+  /* styles omitted... */
+`;
+
+const SlideControl = props => {
+  const MarkdownControl = CMS.getWidget("markdown").control;
+  return (
+    <div>
+      <SlideControlHeader>Slide</SlideControlHeader>
+      <MarkdownControl {...props} />
+    </div>
+  );
+};
+
+const SlidePreview = props => {
+  const MarkdownPreview = CMS.getWidget("markdown").preview;
+  return (
+    <div>
+      <hr />
+      <MarkdownPreview {...props} />
+    </div>
+  );
+};
+
+const separator = "\n\n---n\n";
+
 export class SlidesControl extends Component {
+  getValue() {
+    return this.props.value ? this.props.value : "";
+  }
+
+  handleSlideChange(value, i) {
+    const newValues = this.getValue().split(separator);
+    newValues[i] = value;
+    this.props.onChange(newValues.join(separator));
+  }
+
   render() {
-    return <div>control</div>;
+    const slides = this.getValue().split(separator);
+    const slideControls = slides.map((slideContent, i) => (
+      <SlideControl
+        {...this.props}
+        key={i}
+        value={slideContent}
+        onChange={value => this.handleSlideChange(value, i)}
+      />
+    ));
+    return <div>{slideControls}</div>;
   }
 }
 
-// This is the preview component
-export const SlidesPreview = props => <div>preview</div>;
+export const SlidesPreview = props => (
+  <div>
+    {props.value
+      .split(props.field.get("separator", defaultSeparator))
+      .map((val, i) => <SlidePreview {...props} key={i} value={val} />)}
+  </div>
+);
 
 // const CommandBar = styled.div`
 //   display: flex;
