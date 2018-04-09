c27ee046dd5a08a8edaf1e0db0239f9c3d46f77a
Josh Dobson <joshua@joshuarule.com>
2018-04-09 09:18:03 -0500

    changes about intro to html

--- src/templates/about.js
+++ src/templates/about.js
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "gatsby-link";
 
 export default ({ data }) => (
-	<div>
+  <div>
     <h2>{data.markdownRemark.frontmatter.title}</h2>
     <h2>{data.markdownRemark.frontmatter.subtitle}</h2>
-    <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.frontmatter.body }} />
-  </div>	
+    <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+  </div>
 );
 
 export const query = graphql`
