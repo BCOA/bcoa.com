08de2f2e9b841cc6c9e97390b93cd9402c671130
Josh Dobson <joshua@joshuarule.com>
2018-04-10 13:06:13 -0400

    adds data to about page

--- src/pages/about.md
+++ src/pages/about.md
@@ -21,5 +21,18 @@ studioMembers:
       chicken microdosing iPhone viral keffiyeh typewriter mlkshk.
     memberImage: /images/uploads/placeholder.jpg
     memberName: Luigi
+publications:
+  - title: First Publication 1
+    date: ""
+    url: http://www.google.com
+  - title: Second Publication 2
+    date: ""
+    url: http://www.google.com
+awards:
+  - awardTitle: Award Title 1
+    orgName: Awesome Organization
+    date: ''
+    url: http://www.ikea.com
 ---
+
 Pabst hell of tacos, mustache cred gastropub trust fund gochujang fanny pack. Poutine church-key hell of sustainable, pop-up green juice mumblecore kinfolk adaptogen gochujang. Hexagon tattooed brooklyn godard actually sartorial try-hard PBR&B truffaut. Quinoa retro selfies, meh hot chicken microdosing iPhone viral keffiyeh typewriter mlkshk.
--- src/templates/about.js
+++ src/templates/about.js
@@ -9,29 +9,40 @@ export default ({ data }) => {
     <div>
       <h1>{post.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-      { fields.studioMembers && <ul>
-        { fields.studioMembers.filter(member => member.principal).map((member, i) => (
-          <li key={i}>
-            <h1>TIS A PRINCIPAL</h1>
-            <img src={member.memberImage}/>
-            <h3>{member.memberName}</h3>
-            <p>{member.jobTitle}</p>
-            <p>{member.memberDescription}</p>
-          </li>
-        ))}
+      {fields.studioMembers && (
+        <ul>
+          {fields.studioMembers
+            .filter(member => member.principal)
+            .map((member, i) => (
+              <li key={i}>
+                <h1>TIS A PRINCIPAL</h1>
+                <img src={member.memberImage} />
+                <h3>{member.memberName}</h3>
+                <p>{member.jobTitle}</p>
+                <p>{member.memberDescription}</p>
+              </li>
+            ))}
         </ul>
-      }
-      { fields.studioMembers && <ul>
-        { fields.studioMembers.filter(member => !member.principal).map((member, i) => (
-          <li key={i}>
-            <img src={member.memberImage}/>
-            <h3>{member.memberName}</h3>
-            <p>{member.jobTitle}</p>
-            <p>{member.memberDescription}</p>
-          </li>
-        ))}
+      )}
+      {fields.studioMembers && (
+        <ul>
+          {fields.studioMembers
+            .filter(member => !member.principal)
+            .map((member, i) => (
+              <li key={i}>
+                <img src={member.memberImage} />
+                <h3>{member.memberName}</h3>
+                <p>{member.jobTitle}</p>
+                <p>{member.memberDescription}</p>
+              </li>
+            ))}
         </ul>
-      }
+      )}
+      {fields.publications && (
+        <ul>
+          {fields.publications.map(publication => <h2>{publication.title}</h2>)}
+        </ul>
+      )}
     </div>
   );
 };
