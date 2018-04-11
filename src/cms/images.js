4bc090f04dcfbebc204ab93970c52f7088435b6e
Jake Dobson <jake.dobson@gmail.com>
2018-04-11 15:29:50 -0700

    add image string and text widgets to custom image widget

--- src/cms/images.js
+++ src/cms/images.js
@@ -4,20 +4,28 @@ import styled from "styled-components";
 //editing component
 export class ImagesController extends Component {
 	render() {
-		const URL = CMS.getWidget("string").control;
+		const URL = CMS.getWidget("image").control;
+		const altText = CMS.getWidget("string").control;
+		const caption = CMS.getWidget("text").control;
 		return (
 			<div>
 				<URL {...this.props} />
+				<altText {...this.props} />
+				<caption {...this.props} />
 			</div>
 		);
 	}
 }
 //preview component
 export const ImagesPreview = props => {
-	const URLPreview = CMS.getWidget("string").preview;
+	const URLPreview = CMS.getWidget("image").preview;
+	const altText = CMS.getWidget("string").preview;
+	const caption = CMS.getWidget("text").preview;
 	return (
 		<div>
 			<URLPreview {...props} />
+			<altText {...props} />
+			<caption {...props} />
 		</div>
 	);
 }
