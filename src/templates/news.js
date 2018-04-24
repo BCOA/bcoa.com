3d906eafd3ea81ebe9f8f17a6ac8b25a3ec1a40a
Jake Dobson <jake.dobson@gmail.com>
2018-04-24 10:02:21 -0700

    article collection config -- setup and add articles folder in src/

---
+++ src/templates/news.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "../components/Image";
+
+
+export default ({ data }) => {
+	// const post = data.markdownRemark;
+  // const fields = post.frontmatter;
+  return (
+		<h1>new page</h1>
+		// <div>
+		// 	<h1>{ fields.title }</h1>
+		// 	<div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+		// 	<img src={fields.image} />
+		// 	<h4>{ fields.headline }</h4>
+		// </div>
+  );
+};
+
+export const query = graphql`
+  query NewsPageQuery($slug: String!) {
+    allMarkdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      frontmatter {
+				templateKey
+				title
+				image
+				headline
+				date
+				body
+      }
+    }
+  }
+`;
