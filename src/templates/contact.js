4376195276936d91aee328f34c54eaf9e6019988
Jake Dobson <jake.dobson@gmail.com>
2018-04-23 18:44:24 -0700

    title, heroImage and message display on contact page

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -3,23 +3,28 @@ import Image from "../components/Image";
 
 
 export default ({ data }) => {
+	const post = data.markdownRemark;
+  const fields = post.frontmatter;
   return (
 		<div>
-			<h1>Contact</h1>
+			<h1>{ fields.title }</h1>
+			<div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+			<img src={fields.heroImage} />
+			<h4>{ fields.message }</h4>
 		</div>
   );
 };
 
-// export const query = graphql`
-//   query ContactPageQuery($slug: String!) {
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       id
-//       html
-//       frontmatter {
-//         title
-// 				heroImage
-// 				message
-//       }
-//     }
-//   }
-// `;
+export const query = graphql`
+  query ContactPageQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      frontmatter {
+        title
+				heroImage
+				message
+      }
+    }
+  }
+`;
