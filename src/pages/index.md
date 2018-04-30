0b9089fa41d1c330cb034fb4e5b95002992597aa
Josh Dobson <joshua@joshuarule.com>
2018-04-30 12:08:39 -0400

    fix index queries, adds gallery images

--- src/pages/index.md
+++ src/pages/index.md
@@ -2,7 +2,7 @@
 templateKey: featured-page
 title: Featured
 carouselImages:
-  - altText: Home Page Hero image alt 1
+  - alt: Home Page Hero image alt 1
     caption: Homepage hero image 1 caption
     url: /images/uploads/Full-Size_3X4.png
 ---
--- src/templates/index.js
+++ src/templates/index.js
@@ -3,37 +3,58 @@ import Link from "gatsby-link";
 
 export default ({ data }) => {
   console.log(data);
-  const edges = data.allMarkdownRemark.edges;
+  const edges = data.projects.edges;
   const projects = edges.filter(
     edge =>
       edge.node.frontmatter.templateKey === "project-page" &&
       edge.node.frontmatter.featured
   );
-  console.log(projects);
+  // console.log(projects);
   return (
-    <ul>
-      {projects &&
-        projects.map(({ node: project }, i) => (
-          <li key={i}>
-            <article>
-              <Link to={project.fields.slug}>
-                <img
-                  src={project.frontmatter.previewImage.url}
-                  alt={project.frontmatter.previewImage.alt}
-                />
-                <h1>{project.frontmatter.title}</h1>
-              </Link>
-            </article>
+    <div>
+      <ul>
+        {data.page.frontmatter.carouselImages.map((slide, i) => (
+          <li>
+            <img src={slide.url} alt={slide.alt} />
           </li>
         ))}
-    </ul>
+      </ul>
+      <h2>{data.page.frontmatter.title}</h2>
+      <ul>
+        {projects &&
+          projects.map(({ node: project }, i) => (
+            <li key={i}>
+              <article>
+                <Link to={project.fields.slug}>
+                  <img
+                    src={project.frontmatter.previewImage.url}
+                    alt={project.frontmatter.previewImage.alt}
+                  />
+                  <h1>{project.frontmatter.title}</h1>
+                </Link>
+              </article>
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 };
 
 export const query = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount
+  query IndexQuery($slug: String!) {
+    page: markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      frontmatter {
+        title
+        carouselImages {
+          url
+          alt
+        }
+      }
+    }
+    projects: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
       edges {
         node {
           id
