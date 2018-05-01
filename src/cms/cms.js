6c25a4783a8c1bc64d414e0cc00a2f59ff2b38b5
Josh Dobson <joshua@joshuarule.com>
2018-05-01 13:19:59 -0400

    changes to cms css

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,4 @@
 import CMS from "netlify-cms";
 import { ImagesController, ImagesPreview } from "./images";
-import "./cms.css";
 CMS.registerWidget("images", ImagesController, ImagesPreview);
 CMS.registerPreviewStyle("./cms.css");
