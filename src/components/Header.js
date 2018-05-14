0f6601fd0796714eb791f7b52c8712ffeb05d449
Josh Dobson <joshua@joshuarule.com>
2018-05-14 08:45:44 -0400

    adds menu button toggle

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Link from "gatsby-link";
+
+export default ({ visible, toggleMenu }) => {
+  return (
+    <header>
+      <div className="container">
+        <p style={{ flex: "1" }}>
+          <a href="/">Breitner Ciaccia–Office of Architecture</a>
+        </p>
+        <button className="header-menuButton" onClick={toggleMenu}>menu</button>
+        {visible &&
+          <nav> {/* className="f-navigation" */}
+            <ul>
+              <li>
+                <Link to="/">Featured</Link>
+              </li>
+              <li>
+                <Link to="/work">Index</Link>
+              </li>
+              <li>
+                <Link to="/news">News</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        }
+      </div>
+    </header>
+  )
+}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,85 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Link from "gatsby-link";
 import Helmet from "react-helmet";
 import "../styles/app.scss";
 
-const TemplateWrapper = ({ children, data }) => {
-  console.log(data);
-  return (
-    <div>
-      <Helmet title="Home | BC–OA" />
-      <header className="header">
-        <p style={{ flex: "1" }}>
-          <a href="/">Breitner Ciaccia–Office of Architecture</a>
-        </p>
-        <nav> {/* className="f-navigation" */}
-          <ul>
-            <li>
-              <Link to="/">Featured</Link>
-            </li>
-            <li>
-              <Link to="/work">Index</Link>
-            </li>
-            <li>
-              <Link to="/news">News</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>{children()}</main>
-      <footer>
-        <div className="f-footer-a" style={{ flex: 4 }}>
-          <h2>
-            Breitner<br />Ciaccia–<br />Office of<br />Architecture
-          </h2>
-        </div>
-        <div className="f-footer-b" style={{ flex: 1, padding: "0 1rem" }}>
-          <b>Contact</b>
-          <address className="f-footer-b">
-            { data.contactJson.address.street }
-            <br />
-            { data.contactJson.address.street2 }
-            <br />
-            { data.contactJson.address.city }, { data.contactJson.address.state }{" "}
-            { data.contactJson.address.zip }
-            <br />
-          </address>
-          –
-          <div>
-            <a href={`mailto:${ data.contactJson.email }`}>
-              { data.contactJson.email }
-            </a>
+import Header from "../components/Header";
+
+class TemplateWrapper extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  toggleMenu = () => {
+    this.state.menuOpen ? this.setState({ menuOpen: false }) : this.setState({ menuOpen: true })
+  }
+
+  render() {
+
+    const {
+      children,
+      data
+    } = this.props
+
+    return (
+      <div>
+        <Helmet title="Home | BC–OA" />
+        <Header visible={this.state.menuOpen} toggleMenu={this.toggleMenu} />
+        <main>{children()}</main>
+        <footer>
+          <div className="container">
+            <div className="f-footer-a" style={{ flex: 4 }}>
+              <h2>
+                Breitner<br />Ciaccia–<br />Office of<br />Architecture
+              </h2>
+            </div>
+            <div className="f-footer-b" style={{ flex: 1, padding: "0 1rem" }}>
+              <b>Contact</b>
+              <address className="f-footer-b">
+                {data.contactJson.address.street}
+                <br />
+                {data.contactJson.address.street2}
+                <br />
+                {data.contactJson.address.city}, {data.contactJson.address.state}{" "}
+                {data.contactJson.address.zip}
+                <br />
+              </address>
+              –
+              <div>
+                <a href={`mailto:${data.contactJson.email}`}>
+                  {data.contactJson.email}
+                </a>
+              </div>
+              <div>
+                <a href={`tel:${data.contactJson.phone}`}>
+                  {data.contactJson.phone}
+                </a>
+              </div>
+            </div>
+            <div className="f-footer-b" style={{ flex: 1, padding: "0 1rem" }}>
+              <b>Social</b>
+              <div>
+                <a href={`http://instagram.com/${data.contactJson.instagram}`}>
+                  Instagram
+                </a>
+              </div>
+              <div>
+                <a href={`http://facebook.com/${data.contactJson.facebook}`}>
+                  Facebook
+                </a>
+              </div>
+              <p>@ BC–OA {new Date().getFullYear()}</p>
+            </div>
           </div>
-          <div>
-            <a href={`tel:${ data.contactJson.phone }`}>
-              { data.contactJson.phone }
-            </a>
-          </div>
-        </div>
-        <div className="f-footer-b" style={{ flex: 1, padding: "0 1rem" }}>
-          <b>Social</b>
-          <div>
-            <a href={`http://instagram.com/${data.contactJson.instagram}`}>
-              Instagram
-            </a>
-          </div>
-          <div>
-            <a href={`http://facebook.com/${data.contactJson.facebook}`}>
-              Facebook
-            </a>
-          </div>
-          <p>@ BC–OA {new Date().getFullYear()}</p>
-        </div>
-      </footer>
-    </div>
-  );
+        </footer>
+      </div>
+    );
+  }
 };
 
 export const query = graphql`
