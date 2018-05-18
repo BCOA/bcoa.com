7f83bd2bb79574d2560e6d717cda52f4d44e17e2
Josh Dobson <joshua@joshuarule.com>
2018-05-18 09:02:49 -0400

    adds forked repo for conditional testing

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,4 @@
-import CMS from "netlify-cms";
+import CMS from "netlify-cms/dist/cms";
 import { ImagesController, ImagesPreview } from "./images";
 CMS.registerWidget("images", ImagesController, ImagesPreview);
 CMS.registerPreviewStyle("./cms.css");
