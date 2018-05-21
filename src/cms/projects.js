5d9b10fcc48359d58246b5a67648cc2b947db300
Josh Dobson <joshua@joshuarule.com>
2018-05-21 10:13:04 -0400

    adds headline and body text

--- src/cms/projects.js
+++ src/cms/projects.js
@@ -4,12 +4,15 @@ export default (props) => {
   console.log(props);
   const entry = props.entry;
   const title = entry.getIn(['data', 'title']);
-  // const hero = entry.getIn(['data', 'heroImage']);
   const heroImg = props.getAsset(props.widgetsFor('heroImage').getIn(['data', 'url']));
   return (
     <div>
-      <div>Project Title: {title}</div>
+      <div>{title}</div>
       <img src={heroImg.toString()} alt="hero image" />
+      <div>{entry.getIn(['data', 'headline'])}</div>
+      <div className="marginBottom-5 bp-1_marginBottom-5 bp-2_marginBottom-10"
+        dangerouslySetInnerHTML={{ __html: entry.getIn(['data', 'body']) }}
+      />
     </div>
   )
 };
