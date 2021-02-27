4d244fcc0c9ace2ca2795c912f256d7fac159e38
Joshua Rule Dobson <joshua@joshuarule.com>
2021-02-27 11:30:40 -0500

    resolves 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,30 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import SEO from '../components/SEO'
+import { Link } from "gatsby";
+import React from "react";
+import Layout from "../components/Layout";
+import SEO from "../components/SEO";
 
 const NotFoundPage = (props) => (
-  <Layout {...props} render={() => 
-    <>
-    <SEO />
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-    </>
-  }/>
-)
+  <Layout
+    {...props}
+    render={() => (
+      <div
+        className="container
+          flex-1
+          flex
+          flex-col
+          justify-center
+          marginBottom-15
+          bp-1_marginBottom-24
+          bp-2_marginBottom-20"
+      >
+        <SEO />
+        <h1 className="f-headline-a marginBottom-3">PAGE NOT FOUND</h1>
+        <Link to="/" className="underline">
+          Return home here.
+        </Link>
+      </div>
+    )}
+  />
+);
 
-export default NotFoundPage
+export default NotFoundPage;
